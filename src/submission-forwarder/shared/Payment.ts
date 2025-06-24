319eb9bd2ff37f6af738a30b073990e6c6616804
WietekeNijmegen <wietekenijmegen@gmail.com>
2025-06-24 23:16:16 +0200

    chore: add optional paymentschema to submission

---
+++ src/submission-forwarder/shared/Payment.ts
@@ -0,0 +1,17 @@
+import z from 'zod';
+
+// Possible payment properties from Open Forms
+// For now all optional to test the objecttype
+
+export const PaymentSchema = z
+  .object({
+    payment_completed: z.boolean().optional(),
+    payment_amount: z.number().optional(),
+    payment_public_order_ids: z.array(z.string()).optional(),
+    provider_payment_ids: z.array(z.string()).optional(),
+  })
+  .optional()
+  .nullable();
+
+
+export type Submission = z.infer<typeof PaymentSchema>;
