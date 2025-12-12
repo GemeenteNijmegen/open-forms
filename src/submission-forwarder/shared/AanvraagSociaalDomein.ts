26645988704567f7a7dbc7ff05668374b1eb5621
WietekeNijmegen <wietekenijmegen@gmail.com>
2025-12-12 14:03:13 +0100

    fix: zod still checks if emailadres is an empty string it seems

--- src/submission-forwarder/shared/AanvraagSociaalDomein.ts
+++ src/submission-forwarder/shared/AanvraagSociaalDomein.ts
@@ -33,7 +33,7 @@ const PartnerSchema = z.object({
   Nationaliteit: NationaliteitSchema.optional(),
   Iban: z.string().optional(),
   Telefoonnr: TelefoonSchema.optional(),
-  EMailAdresClient: z.string().email().optional(),
+  EMailAdresClient: z.string().optional(),
 });
 
 const RelatieSchema = z.object({
